4442eed9b4f51c81953ba3ac2bea08a2e7472670
Kirk Groff <kirkgsvc@icloud.com>
2025-12-17 17:10:40 -0500

    added player ship, ship controls, and asteroid field

---
+++ circleshape.py
@@ -0,0 +1,31 @@
+import pygame
+import math
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # must override
+        pass
+
+    def update(self, dt):
+        # must override
+        pass
+
+        distance = self.position 
+        r1 = self.position + self.radius
+        r2 = other.position + other.radius
+        if 1==1:
+            return True
+        
+        return False
--- main.py
+++ main.py
@@ -1,27 +1,51 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from circleshape import CircleShape
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Game init
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()    
     running = True
+    dt = 0
+
+    # Player init
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()    
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
+
+    # Asteroid init
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    # Asteroid Field
+    AsteroidField.containers = (updatable,)
+    asteroidField = AsteroidField()
 
     while running:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
+
+        updatable.update(dt)
         
+        screen.fill("black")
 
+        for obj in drawable:
+            obj.draw(screen)
 
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
